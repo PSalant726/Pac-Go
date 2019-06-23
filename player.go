22001b61ae0bb93043ad34149c5b3a645d60158a
Phil Salant <PSalant@gmail.com>
2019-06-23 17:43:14 -0400

    Allow Player to move

--- player.go
+++ player.go
@@ -1,6 +1,44 @@
 package main
 
 type Player struct {
-	row int
-	col int
+	Row int
+	Col int
+}
+
+func Move(player Player, dir string) {
+	player.Row, player.Col = makeMove(player.Row, player.Col, dir)
+}
+
+func makeMove(oldRow, oldCol int, dir string) (newRow, newCol int) {
+	newRow, newCol = oldRow, oldCol
+
+	switch dir {
+	case "UP":
+		newRow = newRow - 1
+		if newRow < 0 {
+			newRow = len(maze) - 1
+		}
+	case "DOWN":
+		newRow = newRow + 1
+		if newRow == len(maze)-1 {
+			newRow = 0
+		}
+	case "RIGHT":
+		newCol = newCol + 1
+		if newCol == len(maze[0]) {
+			newCol = 0
+		}
+	case "LEFT":
+		newCol = newCol - 1
+		if newCol < 0 {
+			newCol = len(maze[0]) - 1
+		}
+	}
+
+	if maze[newRow][newCol] == '#' {
+		newRow = oldRow
+		newCol = oldCol
+	}
+
+	return
 }
